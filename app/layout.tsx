9b1d531f02adec37eb5f4cc783478caad0878c31
Jnyandeep <thokchom.jnyandeep@gmail.com>
2025-08-07 00:11:47 +0530

    refactor: refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
-  title: "Digital Portfolio",
-  description: "Feel free to explore",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  title: "jny0444",
+  description: "Portfolio",
 };
 
 export default function RootLayout({
@@ -16,7 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <link rel="icon" href="/img.png" type="image/png" sizes="any" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,96 +1,42 @@
-"use client";
-
-import { useState } from "react";
-import Navbar from "@/components/Navbar";
 import Image from "next/image";
-import AboutAndContact from "@/components/About&Contact";
-import CowSay from "@/components/CowSay";
-import Coffee from "@/components/Coffe";
-import Project2025 from "@/components/Project-2025";
-import Project2024 from "@/components/Project-2024";
-import LoaderWrapper from "@/components/LoaderWrapper";
 
 export default function Home() {
-  const [activeItem, setActiveItem] = useState<string | null>(null);
-
   return (
-    <LoaderWrapper>
-      <div>
-        <Navbar activeItem={activeItem} setActiveItem={setActiveItem} />
-
-        <div className="outer-div grid grid-cols-2 md:grid-cols-5 p-5">
-          <div className="text-lg hover:cursor-help">
-            Thokchom Jnyandeep Singh
-          </div>
-          <div className="relative">
-            <Image
-              src="/sonney-rem.png"
-              alt="Sonney"
-              fill
-              className="object-contain rounded-lg"
-              draggable={false}
-            />
-          </div>
-          <AboutAndContact />
-          <CowSay />
-          <div className="hover:cursor-ne-resize">
-            <a
-              href="https://drive.google.com/file/d/1aL4ta6Lz_aRiqDgYuwTWCCMJlC0bWBfe/view?usp=drivesdk"
-              className="hover:cursor-ne-resize"
-            >
-              <p className="cursor-ne-resize">Resume</p>
-              <p className="text-sm cursor-ne-resize">
-                Updated every now and then
-              </p>
-            </a>
-          </div>
-          <div className="relative">
-            <Image
-              src="/(1).png"
-              alt="me fr"
-              fill
-              className="object-contain rounded-lg"
-              draggable={false}
-            />
-          </div>
-          <div className="hover:cursor-help">
-            <p>Languages</p>
-            <ol className="text-sm">
-              <li>Solidity</li>
-              <li>Rust</li>
-              <li>JS</li>
-              <li>C/C++</li>
-            </ol>
-          </div>
-          <div className="hover:cursor-help">
-            <p>Blockchain Tech</p>
-            <ol className="text-sm">
-              <li>Uniswap</li>
-              <li>Hardhat</li>
-              <li>Foundry</li>
-              <li>Wagmi</li>
-            </ol>
-          </div>
-          <div className="hover:cursor-help">
-            <p>Web Tech</p>
-            <ol className="text-sm">
-              <li>Next.js</li>
-              <li>React</li>
-              <li>Typescript</li>
-            </ol>
-          </div>
-          <Coffee />
-          <div className="hover:cursor-copy">
-            <p>Design</p>
-            <p>Heavily inspired by</p>
-            <a href="https://chia.design" className="text-sm underline">
-              chia.design
-            </a>
-          </div>
-          <Project2025 />
-          <Project2024 />
-        </div>
-      </div>
-    </LoaderWrapper>
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      
+      <Image src="/img.png" alt="image" width={300} height={500} />
+      <h1>
+        <b>Thokchom Jnyandeep Singh</b>
+      </h1>
+      <h2>Hi, I am Blockchain dev and I like CS topics</h2>
+      <p>
+        contact me @{" "}
+        <a href="mailto:thokchom.jnyandeep@gmail.com">
+          <i>thokchom.jnyandeep@gmail.com</i>
+        </a>
+      </p>
+      <ul className="flex items-center space-x-2 mt-4">
+        <li>
+          <a href="https://github.com/jny0444">
+            <i>github</i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/thokchom-jnyandeep-singh-54012a288/">
+            <i>linkedin</i>
+          </a>
+        </li>
+        <li>
+          <a href="https://x.com/jny0444">
+            <i>x</i>
+          </a>
+        </li>
+        <li>
+          <a href="https://drive.google.com/file/d/1WTNKJYFPrWsDNTZvwf1WiQJRU3DCD3EZ/view?usp=sharing">
+            <i>resume</i>
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
