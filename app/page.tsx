38e56608a40ee260d3338ca91e5f50d5b34f12fc
Jnyandeep <thokchom.jnyandeep@gmail.com>
2025-11-20 16:01:58 +0530

    feat: home page done

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,8 @@ export default function Home() {
   return (
     <PageShell>
       <div className="flex flex-col-reverse xl:flex-row items-center justify-center xl:min-h-screen w-full">
-        <div className="flex flex-col items-center">
-          <p className=" text-2xl md:text-3xl border-6 p-2 font-bold text-center">
-            Scrolling Kills
-          </p>
-          <p className="text-xl font-light w-52 text-center mt-4 mb-4">
+        <div className="flex flex-col items-center my-10">
+          <p className="text-xl w-52 text-center">
             他们是马，你是独角兽 <br /> 你漂亮，他们丑陋; <br />
             你是天鹅，他们青蛙
           </p>
